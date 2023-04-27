7b0be10b186267bf3cf9c248553f858e8abb08fa
sekha <sekha@NAGENDRASEKHAR>
2023-04-27 18:57:31 +0530

    Test3 script commited

---
+++ src/tests/Test3.java
@@ -0,0 +1,18 @@
+package tests;
+
+public class Test3 {
+@Test
+	
+	public void test()throws Exception{
+		
+		
+		System.out.println("Hello mama");
+		
+		System.out.println("Hyderabad");
+		
+		
+		
+		
+	}
+
+}
